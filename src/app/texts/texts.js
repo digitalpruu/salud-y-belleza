63fc5a5dab0d4c6033ec28cd5404a8c241be3056
kevinpicon01 <kgpicon@hotmail.com>
2024-09-22 18:16:01 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,10 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Salud y Belleza";
+const email = "aracelysierra1972@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    "¡Hola! Estoy muy interesado en los servicios de " + title + " y me gustaría recibir más información sobre limpieza de uñas. ¡Gracias!"
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "+573202150663"; // Incluido el prefijo +57
+
 const textos = {
     index: {
         title: title,
@@ -12,25 +13,25 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "En 'Salud y Belleza' nos especializamos en el cuidado personal, proporcionando servicios de limpieza de uñas de alta calidad. Nos dedicamos a brindarte una experiencia única que realce tu belleza y bienestar.",
+        description2: "Utilizamos productos seleccionados cuidadosamente para ofrecerte resultados impecables, enfocándonos en mejorar tu salud y apariencia a través de un servicio profesional y personalizado. ¡Descubre la experiencia 'Salud y Belleza' y siéntete renovada!"
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
         email: email
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Vida saludable",
+            p2: ""
         },
-        address: "Dirección del emprendimiento",
+        address: "", // Información no proporcionada
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/", // No proporcionado, link genérico
+        instagram: "https://www.instagram.com/", // No proporcionado, link genérico
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
     },
-
 };
+
 export default textos;
