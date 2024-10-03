0d488fe2f63e02966a73a31550ac1e554b397dba
kevinpicon01 <digitalpruu@gmail.com>
2024-10-02 21:23:23 -0500

    Fix

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,4 +1,4 @@
-const title = "Salud y Belleza";
+const title = "BELLEZA NATURAL";
 const email = "aracelysierra1972@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
     "¡Hola! Estoy muy interesado en los servicios de " + title + " y me gustaría recibir más información sobre limpieza de uñas. ¡Gracias!"
@@ -13,8 +13,9 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "En 'Salud y Belleza' nos especializamos en el cuidado personal, proporcionando servicios de limpieza de uñas de alta calidad. Nos dedicamos a brindarte una experiencia única que realce tu belleza y bienestar.",
-        description2: "Utilizamos productos seleccionados cuidadosamente para ofrecerte resultados impecables, enfocándonos en mejorar tu salud y apariencia a través de un servicio profesional y personalizado. ¡Descubre la experiencia 'Salud y Belleza' y siéntete renovada!"
+        description1: "En 'Belleza Natural' nos especializamos en el cuidado personal, proporcionando servicios de alta calidad. Nos dedicamos a brindarte una experiencia única que realce tu belleza y bienestar.\n" +
+            "\n" +
+            "Utilizamos productos seleccionados cuidadosamente para ofrecerte resultados impecables, enfocándonos en mejorar tu salud y apariencia a través de un servicio profesional y personalizado. ¡Descubre la experiencia 'Belleza Natural y siéntete renovada!                 "
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
@@ -28,7 +29,7 @@ const textos = {
         address: "", // Información no proporcionada
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/", // No proporcionado, link genérico
+        facebook: "https://www.facebook.com/profile.php?id=61566393731338&mibextid=ZbWKwL", // No proporcionado, link genérico
         instagram: "https://www.instagram.com/", // No proporcionado, link genérico
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
     },
